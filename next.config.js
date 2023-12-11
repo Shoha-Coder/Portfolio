135b187886bc66bbf581e7eecf23151ac0b5f161
Shoha_Coder <127945777+Shoha-Coder@users.noreply.github.com>
2023-12-11 09:32:34 +0500

    #fix #init #potfolio

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    unoptimized: true,
+  },
+  i18n: {
+    locales: ["en", "fr", "de"],
+    defaultLocale: "en",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
