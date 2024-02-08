52e0bc047e6b36c4328613f8155f1c2d16189571
Shoha_Coder <shohacoder@gmail.com>
2024-02-08 23:49:15 +0500

    #fix #fixed seo

--- src/layout/seo/seo.tsx
+++ src/layout/seo/seo.tsx
@@ -23,22 +23,33 @@ const SEO: React.FC<SeoProps> = ({
         <meta name="author" content={author} />
         <meta name="description" content={metaDescription} />
         <meta name="og:description" content={metaDescription} />
-        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
-        <meta property="og:image:width" content="1280" />
-        <meta property="og:image:height" content="640" />
-        <meta property="og:image" content="https://shoha-coder.uz/opengraph.png" />
-        <meta property="og:image:alt" content="error" />
-        <meta property="twitter:site" content="@shoha_coder" />
         <meta
-          name="google-site-verification"
-          content="LEHoGtjiPhe7nBgOSzpT3Top-nNKx1eaBlllOPQLn8o"
+          name="description"
+          content="Front-end developer (JSX & TSX), web developer. Operational layout. I work with observance of terms and performance of Test tasks."
         />
+        <meta property="og:url" content="https://shoha-coder.uz" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://shoha-coder.uz/" />
-        <meta name="url" content="http://shoha-coder.uz/" />
-
-        <meta property="og:site_name" content="Shoha_Coder website" />
         <meta property="og:title" content="Shoha_Coder - Portfolio" />
+        <meta
+          property="og:description"
+          content="Front-end developer (JSX & TSX), web developer. Operational layout. I work with observance of terms and performance of Test tasks."
+        />
+        <meta
+          property="og:image"
+          content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v3/shoha-coder.uz/Shoha_Coder%20-%20Portfolio/https%3A%2F%2Fshoha-coder.uz%2Fopengraph.png/og.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="shoha-coder.uz" />
+        <meta property="twitter:url" content="https://shoha-coder.uz" />
+        <meta name="twitter:title" content="Shoha_Coder - Portfolio" />
+        <meta
+          name="twitter:description"
+          content="Front-end developer (JSX & TSX), web developer. Operational layout. I work with observance of terms and performance of Test tasks."
+        />
+        <meta
+          name="twitter:image"
+          content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v3/shoha-coder.uz/Shoha_Coder%20-%20Portfolio/https%3A%2F%2Fshoha-coder.uz%2Fopengraph.png/og.png"
+        />
       </Head>
       {children}
     </>
